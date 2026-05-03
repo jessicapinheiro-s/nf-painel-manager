205aa2dc1345f00015a512029a71e521ac4ce28d
Jéssica Pinheiro <jessicasilva.js1314@gmail.com>
2026-05-03 19:18:52 -0300

    ajustando refs incoretas

--- backend/src/config/multer.js
+++ backend/src/config/multer.js
@@ -1,11 +1,12 @@
 import multer from "multer";
+import crypto from "crypto";
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, "uploads/");
   },
   filename: (req, file, cb) => {
-    const hash = crypto.radomBytes(6).toString("hex");
+    const hash = crypto.randomBytes(6).toString("hex");
     const fileName = `${hash}-${file.originalname}`;
     cb(null, fileName);
   },
