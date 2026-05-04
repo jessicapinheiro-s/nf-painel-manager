cacd053130925b0e5e9471aa953ed6ec18664e6a
Jéssica Pinheiro <jessicasilva.js1314@gmail.com>
2026-05-03 21:56:56 -0300

    corrigindo cmd para gerar as tabelas no banco e ref de tabela errada

--- backend/src/repository/users-repository.js
+++ backend/src/repository/users-repository.js
@@ -1,7 +1,7 @@
 import { prisma } from "../../prisma/prisma.js";
 
 export const getUserByEmail = async(user_email) => {
-    return await prisma.users.findUnique({
+    return await prisma.user.findUnique({
         where: {
             email: user_email
         }
